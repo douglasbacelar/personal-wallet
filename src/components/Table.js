1cf72e624cb2343224623e0cc64f75664040985b
Douglas Bacelar <bacelardouglas@outlook.com>
2023-03-11 10:10:30 -0300

    Requisito 6, faltando ajustes de conversão de valores

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,86 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { infoExpenses } = this.props;
     return (
-      <div>Table</div>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {infoExpenses.map((expense) => (
+
+            <tr key={ expense.id }>
+              <td>
+                {' '}
+                {expense.description}
+              </td>
+              <td>
+                {' '}
+                {expense.tag}
+              </td>
+              <td>
+                {' '}
+                {expense.method}
+              </td>
+              <td>
+                {' '}
+                {expense.value}
+              </td>
+              <td>
+                {' '}
+                BRL
+              </td>
+              <td>
+                {' '}
+                Verificar
+              </td>
+              <td>
+                {' '}
+                Verificar
+              </td>
+              <td>
+                {' '}
+                {expense.currency}
+              </td>
+              <td>
+                {' '}
+                Verificar
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  infoExpenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  infoExpenses: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    value: PropTypes.string,
+    description: PropTypes.string,
+    currency: PropTypes.string,
+    method: PropTypes.string,
+    tag: PropTypes.string,
+  })).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,46 +1,18 @@
 import React from 'react';
-import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
-    // const { infoExpenses } = this.props;
     return (
       <div>
         <Header />
         <WalletForm />
-        {/* <main>
-          {infoExpenses.map((expense) => (
-            <div key={ expense.id }>
-              <p id="value">{expense.id}</p>
-              <p id="value">{expense.value}</p>
-              <p id="value">{expense.description}</p>
-              <p id="value">{expense.currency}</p>
-              <p id="value">{expense.method}</p>
-              <p id="value">{expense.tag}</p>
-            </div>
-          ))}
-        </main> */}
-
+        <Table />
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  infoExpenses: state.wallet.expenses,
-});
-
-// Wallet.propTypes = {
-//   infoExpenses: PropTypes.arrayOf(PropTypes.shape({
-//     id: PropTypes.number,
-//     value: PropTypes.string,
-//     description: PropTypes.string,
-//     currency: PropTypes.string,
-//     method: PropTypes.string,
-//     tag: PropTypes.string,
-//   })).isRequired };
-
-export default connect(mapStateToProps)(Wallet);
+export default Wallet;
